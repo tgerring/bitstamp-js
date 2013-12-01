41431c88ce9e6090c48fe0d67207e27f4e3b2633
Taylor Gerring <taylor.gerring@gmail.com>
2013-12-01 15:21:24 +0100

    Initial commit

---
+++ bitstamp-js.js
@@ -0,0 +1,136 @@
+/*
+
+Depends:
+* jssha256.js
+* jquery.js
+
+Usage:
+var bitstamp = new Bitstamp('clientid', 'apikey', 'apisecret');
+bitstamp.submitRequest(bitstamp.methods.ticker, function(data){console.log(data);} );
+bitstamp.submitRequest(bitstamp.methods.cancelorder, function(data){console.log(data);}, {id: 1} );
+
+*/
+
+Bitstamp = function(client_id, api_key, api_secret) {
+  this.auth = {client_id: client_id, api_key: api_key, api_secret: api_secret};
+  this.host = 'https://www.bitstamp.net';
+  this.methods = {
+    ticker: {
+        endpoint: '/api/ticker/',
+        method: 'GET'
+    },
+    orderbook: {
+        endpoint: '/api/order_book/',
+        method: 'GET',
+        params: ['group']
+    },
+    transactions: {
+        endpoint: '/api/transactions/',
+        method: 'GET',
+        params: ['time']
+    },
+    eurusd: {
+        endpoint: '/api/eur_usd/',
+        method: 'GET'
+    },
+    balance: {
+        endpoint: '/api/balance/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce']
+    },
+    usertransactions: {
+        endpoint: '/api/user_transactions/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce', 'offest', 'limit', 'sort']
+    },
+    openorders: {
+        endpoint: '/api/open_orders/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce']
+    },
+    cancelorder: {
+        endpoint: '/api/cancel_order/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce', 'id']
+    },
+    orderbuy: {
+        endpoint: '/api/buy/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce', 'price', 'amount']
+    },
+    ordersell: {
+        endpoint: '/api/sell/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce', 'price', 'amount']
+    },
+    withdrawalrequests: {
+        endpoint: '/api/withdrawal_requests/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce']
+    },
+    btcwithdrawal: {
+        endpoint: '/api/bitcoin_withdrawal/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce', 'amount', 'address']
+    },
+    btcdepositaddress: {
+        endpoint: '/api/bitcoin_deposit_address/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce']
+    },
+    unconfirmedbtc: {
+        endpoint: '/api/unconfirmed_btc/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce']
+    },
+    ripplewithdrawal: {
+        endpoint: '/api/ripple_withdrawal/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce', 'amount', 'address', 'currency']
+    },
+    rippledepositaddress: {
+        endpoint: '/api/ripple_address/',
+        method: 'POST',
+        params: ['key', 'signature', 'nonce']
+    }
+  }
+}
+
+Bitstamp.prototype.submitRequest = function(bitstampmethod, callback, params) {
+  if (!params)
+    params = {};
+
+  if ($.inArray('signature', bitstampmethod.params) > -1 && !('signature' in bitstampmethod.params) ) {
+    console.log('Signature required but not in supplied params');
+
+    unix_timestamp = Math.round(+new Date() / 1000);
+    message = unix_timestamp.toString() + this.auth.client_id + this.auth.api_key;
+    signature = HMAC_SHA256_MAC(this.auth.api_secret, message).toUpperCase();
+
+    params.key = this.auth.api_key;
+    params.signature = signature;
+    params.nonce = unix_timestamp;
+  }
+
+  for (var param in params) {
+    if (typeof params[param] == 'undefined') {
+      delete params[param];
+    } else {
+      params[param] = params[param].toString();
+    }
+  }
+
+  console.log('Submitting request');
+
+  $.ajax({
+    type: bitstampmethod.method,
+    url: bitstamp.host + bitstampmethod.endpoint,
+    data: params,
+    success: callback,
+    dataType: 'json'
+  });
+
+  return params;
+}
+
+
---
+++ tests.js
@@ -0,0 +1,123 @@
+var bitstamp = new Bitstamp('clientid', 'api_key', 'api_secret');
+
+function displayInputParams(params) {
+  $('#params').val(JSON.stringify(params));
+}
+
+function debugResponse(data, status) {
+  console.log('Response returned');
+  console.log(data);
+
+  if ('error' in data) {
+    var errorstring = '';
+    if (typeof data.error === 'string') {
+      errorstring = data.error;
+    } else {
+      for (var key in data.error) {
+        errorstring += data.error[key] + '\n';
+      }
+    }
+    $('#response').val('Bitstamp returned error: ' + errorstring);
+    return;
+  }
+  $('#response').val(JSON.stringify(data));
+}
+
+
+
+
+function getTicker() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.ticker, debugResponse);
+  displayInputParams(params);
+}
+
+function getOrderBook() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.orderbook, debugResponse);
+  displayInputParams(params);
+}
+
+function getTransactions(group) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.transactions, debugResponse, {group: group});
+  displayInputParams(params);
+}
+
+function getEurUsdRate() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.eurusd, debugResponse);
+  displayInputParams(params);
+}
+
+function getBalance() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.balance, debugResponse);
+  displayInputParams(params);
+}
+
+function getUserTransactions(offset, limit, sort) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.usertransactions, debugResponse, {offset: offset, limit: limit, sort: sort});
+  displayInputParams(params);
+}
+
+function getOpenOrders() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.openorders, debugResponse);
+  displayInputParams(params);
+}
+
+function cancelOrder(orderid) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.cancelorder, debugResponse, {id: orderid});
+  displayInputParams(params);
+}
+
+function orderBuy(amount, price) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.orderbuy, debugResponse, {amount: amount, price: price});
+  displayInputParams(params);
+}
+
+function orderSell(amount, price) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.ordersell, debugResponse, {amount:amount, price: price});
+  displayInputParams(params);
+}
+
+function withdrawalRequest() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.withdrawalrequests, debugResponse);
+  displayInputParams(params);
+}
+
+function bitcoinWithdrawal(amount, address) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.btcwithdrawal, debugResponse, {amount: amount, address: address});
+  displayInputParams(params);
+}
+
+function getBitcoinDepositAddress() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.btcdepositaddress, debugResponse);
+  displayInputParams(params);
+}
+
+function listUnconfirmedBitcoinTransactions() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.unconfirmedbtc, debugResponse);
+  displayInputParams(params);
+}
+
+function rippleWithdrawal(amount, address, currency) {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.ripplewithdrawal, debugResponse, {amount: amount, address: address, currency: currency});
+  displayInputParams(params);
+}
+
+function getRippleDepositAddress() {
+  $('#response').val('');
+  params = bitstamp.submitRequest(bitstamp.methods.rippledepositaddress, debugResponse);
+  displayInputParams(params);
+}
